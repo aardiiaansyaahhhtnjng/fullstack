df76fd74d56c09b16714be08fc9482ffc1306781
aardiiaansyaahhhtnjng <ardiansyah070702@gmail.com>
2024-09-24 23:12:08 +0700

    new update

--- backend/config/UserDatabase.js
+++ backend/config/UserDatabase.js
@@ -1,22 +1,3 @@
-// // import { Sequelize } from "sequelize"
-// import mysql from "mysql2/promise";
-
-// // const db = new Sequelize('crud_db', 'root', '4Bersaudara_', {
-// //     host: '172.17.0.3',
-// //     dialect: 'mysql',
-// //     port: 1122
-// // });
-
-// const db = mysql.createPool({
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     port: process.env.DB_PORT,
-//     host: process.env.DB_HOST
-// }); 
-
-// export default db;
-
 import { Sequelize } from "sequelize";
 import { config } from "dotenv";
 
