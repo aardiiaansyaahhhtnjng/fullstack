9075811965eee0f56502c429f37130ca60254633
aardiiaansyaahhhtnjng <ardiansyah070702@gmail.com>
2024-09-25 00:15:22 +0700

    update ignore

--- backend/config/UserDatabase.js
+++ backend/config/UserDatabase.js
@@ -6,7 +6,7 @@ config();
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    port: process.env.DB_PORT || 3306 // port default MySQL
+    port: process.env.DB_PORT
 });
 
 export default db;
